46bfd485f3760b185b38f23bbfe41e7b6ca00e62
Kettil Wigenstedt <kettil.brochmann@gmail.com>
2020-10-28 14:52:09 +0100

    Changed main to flip the LED from off to on.

--- main.c
+++ main.c
@@ -8,12 +8,17 @@
 #include "serial.h"
 #include "timer.h"
 
-void main (void) {
+int main (void) {
 	uart_init();
-
+	LED_init();
+	
 	while (1) {
 		/* remove this once you've verified it works */
-		printf_P(PSTR("Hello there\n"));
+		//printf_P(PSTR("Hello there\n"));
+		PORTB |= (1 << 3);
+		_delay_ms(1000);
+		PORTB &= ~(1 << 3);
 		_delay_ms(1000);
 	}
+	return 0;
 }
