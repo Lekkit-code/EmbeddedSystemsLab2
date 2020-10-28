c92ae5ca9136b41c1bcb4c257be9d6298192521f
Kettil Wigenstedt <kettil.brochmann@gmail.com>
2020-10-28 18:32:18 +0100

    Changed main to flip the light 10 times/second. The formula for getting the value we compare to is OCR0A=((clock frequency/prescaler)*(wanted frequency/second)).

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
 #include <stdio.h>
 
@@ -8,17 +9,30 @@
 #include "serial.h"
 #include "timer.h"
 
+volatile int counter = 0; //Needs to be volatile to work with interrupts.
+
 int main (void) {
 	uart_init();
 	LED_init();
+	timer_init();
+
+	sei(); //Enables interrupts.
 	
 	while (1) {
-		/* remove this once you've verified it works */
-		//printf_P(PSTR("Hello there\n"));
-		PORTB |= (1 << 3);
-		_delay_ms(1000);
-		PORTB &= ~(1 << 3);
-		_delay_ms(1000);
+		
+		if (counter == 10)//Every 10th interrupt we turn our LED on/off.
+		{
+			counter = 0;
+			LED_flip();
+		}
 	}
 	return 0;
+}
+
+
+
+ISR(TIMER0_COMPA_vect) //Triggers when TCNT0 and OCR0A have the same value, which happens every 10th ms. Adds one to the counter and then clears the interrupt flag.
+{
+    counter++;
+    TIFR0 &= ~(1 << OCF0A);
 }
