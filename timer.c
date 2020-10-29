33c020d4f6def5db0fe7f9872ca9323c9f4fd3b3
Kettil Wigenstedt <kettil.brochmann@gmail.com>
2020-10-29 17:57:13 +0100

    Changed timer_init function to Fast PWM with a prescaler of 64. This results in a 976,6hz frequency. The frequency is (clock frequency / (prescaler*256)).

--- timer.c
+++ timer.c
@@ -1,13 +1,8 @@
 #include <avr/io.h>
-
 #include "timer.h"
-#define MYOCR0A ((16000000/1024)*0.01) //Since we're running at 16Mhz with a prescaler of 1024 we want to find out how many times/second the counter will increment. We take that value and multiply it by the amount we want/second to get the value we want in our ORC0A, which is the value we'll be comparing to.
 
 void timer_init() {
-	TCCR0A |= (1 << WGM01); //Set the mode of TC0 to CTC.
-	TCCR0B |= (1 << CS02) | (1 << CS00);	//Set the prescaler to 1024.
-	OCR0A = MYOCR0A;	//Set the value to count to to MYOCR0A.
-	TIMSK0 |= (1 << OCIE0A);	//Enable the ISA COMPA vector.
-	
+	TCCR0A |= (1 << WGM01) | (1<< WGM00) | (1<<COM0A1); //Set the mode of TC0 to Fast PWM non-inverted mode.
+	TCCR0B |= (1 << CS01) | (1 << CS00);	//Set the prescaler to 64.
 }
 
